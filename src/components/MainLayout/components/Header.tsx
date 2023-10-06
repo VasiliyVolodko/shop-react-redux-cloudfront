707a191915ba804e851c0de4a4de469faebe09d2
Vasiliy Volodko <Vasiliy_Volodko@epam.com>
2023-10-06 18:42:49 +0200

    task-2

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            My Store! EPAM-lessons
           </Link>
         </Typography>
 
