89a35cd61be7af502c83f01096e01e460d9c13d9
Vasiliy Volodko <Vasiliy_Volodko@epam.com>
2023-10-08 12:38:50 +0200

    task-3 update path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  products: "https://66lblb7fkj.execute-api.eu-west-1.amazonaws.com/dev",
+  products: "https://n7n8t60kf0.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
