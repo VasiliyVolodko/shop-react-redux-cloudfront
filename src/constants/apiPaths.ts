84029941e19c7545bb3cad318a860333b53cdfca
Vasiliy Volodko <Vasiliy_Volodko@epam.com>
2023-11-03 15:10:54 +0100

    task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   products: "https://n7n8t60kf0.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://adsad5n6th.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://n7n8t60kf0.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
