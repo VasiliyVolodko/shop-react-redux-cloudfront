31ed3b7f1a45cd9f01fb41a58f8c0ffa8da9b3e0
Vasiliy Volodko <Vasiliy_Volodko@epam.com>
2024-02-04 19:21:50 +0100

    taask-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://apim-product-service-vv.azure-api.net/func-app-product-service",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://apim-product-service-vv.azure-api.net/import-service",
   bff: "https://apim-product-service-vv.azure-api.net/func-app-product-service",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
